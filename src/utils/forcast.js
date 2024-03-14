c9debeb51806cd2338a686e728c6fd1c5d59925f
Yahia Feteiha <yahiafeteiha@Yahias-MacBook-Pro.local>
2024-03-14 15:06:36 +0200

    Init commit

---
+++ src/utils/forcast.js
@@ -0,0 +1,17 @@
+const request = require('postman-request')
+
+const forcast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=66d722d522d219dda1cebe806c6cd993&query=' + latitude + ',' + longitude + '&units=m'
+    
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        }else if (body.error) {
+            callback('Unable to find location', undefined)
+        }else{
+            callback(undefined, body.current.weather_descriptions[0] + ". It is current "+ body.current.temperature + " degress out." + "It feels like " + body.current.feelslike + " degress out.")
+        }
+    })
+}
+
+module.exports =forcast
